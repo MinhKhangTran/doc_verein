649ec11604eff2d0f60fb15372cc900c3c1e865b
MinhKhangTran <minhkhang.tran@gmx.de>
2022-08-09 06:02:24 +0200

    Initial commit

---
+++ docs/intro.md
@@ -0,0 +1,16 @@
+---
+slug: /
+title: Start
+---
+
+# Der Verein
+
+I'm baby art party mlkshk irony tbh kickstarter mixtape selvage jean shorts vinyl farm-to-table letterpress biodiesel single-origin coffee bespoke. Tacos fanny pack pok pok jianbing, yuccie migas freegan asymmetrical chicharrones. Brooklyn pork belly organic crucifix lomo kale chips. Portland kale chips unicorn gluten-free.
+
+Fit flexitarian gatekeep mlkshk street art shabby chic PBR&B. Heirloom master cleanse hashtag, whatever seitan knausgaard lumbersexual prism scenester ennui PBR&B direct trade raw denim. VHS tattooed woke, pitchfork tumeric flannel raw denim blue bottle. Gochujang chillwave succulents authentic mumblecore. DSA woke fixie tote bag, swag brunch meditation small batch farm-to-table beard chambray. Direct trade yes plz celiac banjo viral occupy waistcoat shabby chic you probably haven't heard of them kombucha succulents freegan.
+
+## Das bieten wir Ihnen an
+
+Letterpress unicorn selvage, ascot big mood pork belly vexillologist affogato meggings kickstarter paleo. Small batch fixie kogi, affogato street art freegan sriracha mixtape edison bulb ascot plaid 8-bit YOLO poke. Portland tousled quinoa kale chips. Vegan biodiesel lumbersexual cliche.
+
+Yr cornhole tilde hashtag raw denim, blog same 3 wolf moon. Microdosing la croix meh marfa salvia. Keffiyeh unicorn lyft chambray plaid marfa aesthetic VHS ugh pug wolf williamsburg. Aesthetic kitsch next level selvage man bun cold-pressed.
---
+++ docusaurus.config.js
@@ -0,0 +1,98 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Der Verein",
+  tagline: "Dein lokaler Sportverein",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+        },
+
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Der Verein",
+
+        items: [{ to: "/kontakt", label: "Kontakt", position: "right" }],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Der Verein",
+            items: [
+              {
+                label: "Start",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Der Verein`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
