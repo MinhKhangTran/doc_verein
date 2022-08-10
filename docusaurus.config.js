7eae6c842cf8c93a138c568fa6fdc9c368e65496
Minh Khang Tran <67480568+MinhKhangTran@users.noreply.github.com>
2022-08-10 15:42:42 +0200

    :sparkles: add all termine in one

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,6 +29,8 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarTitle: 'Alle Termine',
+          blogSidebarCount: 'ALL',
         },
 
         theme: {
